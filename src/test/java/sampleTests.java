9588bc8172991b0d0a737edd9690cf45ce7642fc
neha1412 <nehakum287@gmail.com>
2022-01-28 17:08:05 +0530

    Rest API 3rd assignment to create a user

--- src/test/java/sampleTests.java
+++ src/test/java/sampleTests.java
@@ -1,3 +1,4 @@
+import io.restassured.http.ContentType;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -15,5 +16,25 @@ public class sampleTests {
                 .then()
                 .statusCode(200)
                 .log().body();
+    }
+
+    @Test
+    public void shouldCreateUsers() {
+
+        given()
+                .accept(ContentType.JSON)
+                .contentType(ContentType.JSON)
+                .header("Authorization", "Bearer 7af240fb7b557b1314869bc519303f4e79eb90f287818ef0c9b01eef8e85f17a")
+                .body("{\n" +
+                        "  \"name\": \"Tenali Rama\", \n" +
+                        " \"gender\": \"male\", \n" +
+                        " \"email\": \"tenali.rama12@gmail.com\",\n" +
+                        " \"status\": \"active\"\n" +
+                        "}")
+                .when()
+                .post("https://gorest.co.in/public/v1/users")
+                .then()
+                .log().body()
+                .statusCode(201);
     }
 }
