2262228e5a80cfbfa9412e9887db9f3c7a963329
neha1412 <nehakum287@gmail.com>
2022-01-28 13:51:13 +0530

    Rest API 1st assignment commit

---
+++ src/test/java/sampleTests.java
@@ -0,0 +1,11 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class sampleTests {
+
+    @Test
+    public void shouldTestsomething()
+    {
+        Assert.assertEquals(1, 1);
+    }
+}
