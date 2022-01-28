f875d338b5429e6560dcb2f8825cfe0f41dd3a15
neha1412 <nehakum287@gmail.com>
2022-01-28 15:31:24 +0530

    Rest API 2nd assignment to get all users

--- src/test/java/sampleTests.java
+++ src/test/java/sampleTests.java
@@ -1,11 +1,19 @@
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import static io.restassured.RestAssured.given;
+
 public class sampleTests {
 
     @Test
-    public void shouldTestsomething()
+    public void toGetAllUsers()
     {
-        Assert.assertEquals(1, 1);
+
+        given()
+                .when()
+                .get("https://gorest.co.in/public/v1/users")
+                .then()
+                .statusCode(200)
+                .log().body();
     }
 }
